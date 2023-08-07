821e514ead718a3f53875a203fad23f9eb27f5fd
FedeIneco <federico.diaz@ineco.com>
2023-08-07 15:19:03 +0200

    server side

---
+++ server/app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const app = express();
+const dbService = require("./dbService");
+const { response } = require("express");
+dotenv.config();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//Read
+app.get("/getAll", (request, response) => {
+  const db = dbService.getDbServiceInstance();
+  const page = request.query.page || 1;  
+  const result = db.getAllData(page);
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log(err));
+});
+
+//Get Data
+app.get("/getDetails/:id", async (request, response) => {
+  const elementId = request.params.id;   
+  try {
+    const db = dbService.getDbServiceInstance();
+    const element = await db.getElementById(elementId);    
+    if (!element) {
+      return response.status(404).json({ message: "Elemento no encontrado" });
+    }
+    return response.status(200).json(element);
+  } catch (error) {
+    return response.status(500).json({ message: "Error en el servidor" });
+  }
+});
+
+//Update
+app.patch('/update', async (request, response) => {
+  const { id, status } = request.body;
+  const db = dbService.getDbServiceInstance();
+
+  try {
+    const result = await db.updateById(id, status);
+    response.json({ data: result });
+  } catch (error) {
+    console.log(error);
+    response.status(500).json({ message: "Error en el servidor al actualizar el registro." });
+  }
+});
+
+//Delete
+app.delete('/delete/:id', async (request, response) => {
+  const { id } = request.params;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.deleteRowById(id);
+  result
+    .then((data) => response.json({ success: data }))
+    .catch((err) => console.log(err));
+})
+
+
+//Search
+app.get('/search/:orgc', (request, response) => {
+  const { orgc } = request.params;
+  const db = dbService.getDbServiceInstance();
+
+  const result = db.searchByOrgc(orgc);
+  result
+    .then((data) => response.json({ data: data }))
+    .catch((err) => console.log(err));
+})
+
+app.listen(process.env.PORT, () => console.log("app is running"));
---
+++ server/dbService.js
@@ -0,0 +1,138 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+const { query } = require("express");
+let instance = null;
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.DB_PORT,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err.message);
+  }
+  console.log("db " + connection.state);
+});
+
+class DbService {
+  static getDbServiceInstance() {
+    return instance ? instance : new DbService();
+  }
+
+  async getAllData(page) {
+    try {
+      const pageSize = 25;
+      const offset = Math.max(0, (page - 1) * pageSize);
+      const response = await new Promise((resolve, reject) => {
+        const query =
+          "SELECT * FROM `observatorio` ORDER BY `fechaExtraccion` DESC LIMIT ? OFFSET ?";
+        connection.query(query, [pageSize, offset], (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async getElementById(id) {
+    try {
+      const response = await new Promise((resolve, reject) => {
+        const query = "SELECT * FROM `observatorio` WHERE `id`= ?";
+        const values = [id];
+
+        connection.query(query, values, (err, results) => {
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async deleteRowById(id) {
+    try {
+      id = parseInt(id, 10);
+  
+      const response = await new Promise((resolve, reject) => {
+        connection.beginTransaction((err) => {
+          if (err) reject(new Error(err.message));
+  
+          const deleteQueryCpv = "DELETE FROM cpv WHERE id_observatorio = ?";
+          const deleteQueryObservatorio = "DELETE FROM observatorio WHERE id = ?";
+  
+          connection.query(deleteQueryCpv, [id], (err, result) => {
+            if (err) {
+              connection.rollback(() => {
+                reject(new Error(err.message));
+              });
+            }
+  
+            connection.query(deleteQueryObservatorio, [id], (err, result) => {
+              if (err) {
+                connection.rollback(() => {
+                  reject(new Error(err.message));
+                });
+              }
+  
+              connection.commit((err) => {
+                if (err) {
+                  connection.rollback(() => {
+                    reject(new Error(err.message));
+                  });
+                }
+  
+                resolve(result);
+              });
+            });
+          });
+        });
+      });      
+      return response.affectedRows === 1 ? true : false;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  
+  async updateById(id, status) {
+    try {
+      id = parseInt(id, 10);
+      const response = await new Promise((resolve, reject) => {
+        const query = "UPDATE observatorio SET status = ? WHERE id = ?";
+        connection.query(query, [status, id], (err, result) => {
+          if (err) reject(new Error(err.message));
+          resolve(result);
+        });
+      });
+      return { success: true, data: response };
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+  async searchByOrgc(name){
+    const response = await new Promise((resolve, reject) => {
+        const query =
+          "SELECT * FROM `observatorio`  WHERE organoContratacion LIKE CONCAT('%', ?, '%') ORDER BY `fechaExtraccion` DESC";
+        connection.query(query, [name], (err, results) => {            
+          if (err) reject(new Error(err.message));
+          resolve(results);
+        });
+      });
+      return response;
+    } catch (error) {
+      console.log(error);
+  }
+}
+
+module.exports = DbService;
